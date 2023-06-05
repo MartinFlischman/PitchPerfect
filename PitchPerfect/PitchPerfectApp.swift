0c3e8b92978f481f67afd9ec67bb3007a4fe2ac7
MartinFlischman <mflischman@icloud.com>
2023-06-05 18:03:04 +0200

    Initial Commit

---
+++ PitchPerfect/PitchPerfectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchPerfectApp.swift
+//  PitchPerfect
+//
+//  Created by Martin on 2023/06/05.
+//
+
+import SwiftUI
+
+@main
+struct PitchPerfectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
